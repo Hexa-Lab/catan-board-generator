b7ac341927a0e63e31f1a5d18dafa64f90873fdb
Jake Krummenacher <94008106+JakeKrummenacher@users.noreply.github.com>
2024-02-03 19:03:47 -0700

    Revert "Revert "Merge branch 'main' into button-swap-updates""
    This reverts commit 6be1a1edde36387d27c6bfccaa777229f7759e29.

---
+++ client/src/components/DiceDisplay.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DiceDisplay = ({ diceRollResult, eventDieResult, isCitiesAndKnights }) => {
+  if (!diceRollResult) return null; // Do not render anything if there's no result
+
+  return (
+    <div style={{ position: 'absolute', bottom: '100px', right: '100px', display: "flex", width: isCitiesAndKnights ? "225px" : "150px", justifyContent: "space-between"}}>
+      <img src={`/assets/images/dice/red/${diceRollResult.dice1}.png`} style={{width: "70px"}} alt="Red Dice" />
+      <img src={`/assets/images/dice/yellow/${diceRollResult.dice2}.png`} style={{width: "70px"}} alt="Yellow Dice" />
+      {isCitiesAndKnights && <img src={`/assets/images/dice/event/${eventDieResult}.png`} style={{width: "70px"}} alt="Event Dice" />}
+    </div>
+  );
+};
+
+export default DiceDisplay;
